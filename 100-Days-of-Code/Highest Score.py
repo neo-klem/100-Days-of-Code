e70e5af7abf4dad9fe4b1f2f1ce8f1a7aae76e26
neo-klem <servisracunaramaya@gmail.com>
2025-12-17 16:40:29 +0100

    One more.

---
+++ 100-Days-of-Code/Highest Score.py
@@ -0,0 +1,9 @@
+students = [150,132,32,56,34,76,85,44,33,154]
+
+highest_score = 0
+
+for student in students:
+    if student > highest_score:
+        highest_score = student
+
+print(highest_score)
