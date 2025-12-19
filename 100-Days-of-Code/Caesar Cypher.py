e29bea0197add69b6c05214074a355ffb7560532
neo-klem <servisracunaramaya@gmail.com>
2025-12-19 18:41:36 +0100

    Slava.

--- 100-Days-of-Code/Caesar Cypher.py
+++ 100-Days-of-Code/Caesar Cypher.py
@@ -1,17 +1,26 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x','y', 'z']
 
-def encrypt(original_text, shift_amount):
-    encrypted_text = ""
-    for i in original_text:
-        z = alphabet.index(i) + shift_amount
-        if z >= 26:
-            x = z - 26
-            encrypted_text += alphabet[x]
-        else:
-            encrypted_text += alphabet[alphabet.index(i) + shift_amount]
-    print(encrypted_text)
-# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = "zoran" # input("Type your message:\n").lower()
-shift = 1 # int(input("Type the shift amount:\n"))
+def caesar(direction, original_text, shift_amount):
+    while True:
+        if direction == "encode":
+            encrypted_text = ""
+            for i in original_text:
+                z = alphabet.index(i) + shift_amount
+                if z >= 26:
+                    x = z - 26
+                    encrypted_text += alphabet[x]
+                else:
+                    encrypted_text += alphabet[z]
+            print(f"{encrypted_text}")
+        elif direction == "decode":
+            decrypted_text = ""
+            for i in original_text:
+                z = alphabet.index(i) - shift_amount
+                decrypted_text += alphabet[z]
+            print(decrypted_text)        
 
-encrypt(text, shift)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = "mxcbkoqfq" # input("Type your message:\n").lower()
+shift = 2 # int(input("Type the shift amount:\n"))
+
+caesar(direction, text, shift)
--- testing-grounds.py
+++
@@ -1,5 +0,0 @@
-fruits = ['apple', 'banana', 'cherry']
-
-x = fruits.index("cherry") + 1
-
-print(x)
